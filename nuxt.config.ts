900077da1189f8a5931d6b89a1302d83f9b9c53d
Mamatov Umar <mamatov.umar@mail.ru>
2023-12-22 13:36:03 +0500

    feat: pinia added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: false,
+
+    timeline: {
+      enabled: true
+    }
+  },
   css: [
     '~/assets/styles/main.scss'
   ],
   modules: [
-    '@element-plus/nuxt'
-  ]
+    '@element-plus/nuxt',
+    '@pinia/nuxt'
+  ],
+  runtimeConfig: {
+    public: {
+      baseUrl: '/api'
+    }
+  }
 })
